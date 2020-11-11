20164facf6e90ddb682d2cf78d54a45b9e2aa65c
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2020-11-11 18:05:06 +0100

    Publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule MixBitbake.MixProject do
 
   def project do
     [
+      description: "Generates a bitbake recipe from Elixir application",
       app: :mix_bitbake,
       version: "0.1.0",
       elixir: "~> 1.10",
@@ -21,13 +22,15 @@ defmodule MixBitbake.MixProject do
     [
       files: ["lib", "priv", "mix.exs", "README.md", "LICENSE", "CHANGELOG.md"],
       maintainers: ["João Henrique Ferreira de Freitas"],
-      licenses: ["Apache-2"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/meta-erlang/mix_bitbake"}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
